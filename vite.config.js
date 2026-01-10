aad455d813eb16cc3549e7dfb047db1b6a6155cf
syyutud <veroni0ty@gmail.com>
2026-01-10 15:30:45 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
+//export default defineConfig({
+ // base: "/",
+  //plugins: [svelte(), viteSingleFile() /*, splitVendorChunkPlugin()*/],
+//});
+
+
 export default defineConfig({
-  base: "/",
-  plugins: [svelte(), viteSingleFile() /*, splitVendorChunkPlugin()*/],
+  base: "./",
+  plugins: [svelte(), viteSingleFile()],
 });
